bc3f5fbbc6d4ed4e25bce6751a724359498d6781
BirkhoffLee <github@birkhoff.me>
2018-02-26 10:45:53 +0800

    Clone from JimmyLv/personal-reading-flow

---
+++ index.js
@@ -0,0 +1,14 @@
+const Express = require('express')
+const Webtask = require('webtask-tools')
+const bodyParser = require('body-parser')
+const app = Express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// yet to be created
+// app.use(require('./middlewares/db').connectDisconnect)
+// require('./routes/stories')(app)
+require('./routes/reading')(app)
+
+module.exports = Webtask.fromExpress(app)
---
+++ routes/stories.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const Story = require('../models/Story')
+
+module.exports = (app) => {
+  app.get('/stories', (req, res) => {
+    req.storyModel.find({}).sort({ 'created_at': -1 }).exec((err, stories) => res.json(stories))
+  })
+
+  app.post('/stories', (req, res) => {
+    const newStory = new req.storyModel(Object.assign({}, req.body, { created_at: Date.now() }))
+    newStory.save((err, savedStory) => {
+      if (err) console.error(err)
+      res.json(savedStory)
+    })
+  })
+
+  app.put('/stories', (req, res) => {
+    const idParam = req.webtaskContext.query.id
+    req.storyModel.findOne({ _id: idParam }, (err, storyToUpdate) => {
+      const updatedStory = Object.assign(storyToUpdate, req.body)
+      updatedStory.save((err, story) => {
+        if (err) console.error(err)
+        res.json(story)
+      })
+    })
+  })
+
+  app.delete('/stories', (req, res) => {
+    const idParam = req.webtaskContext.query.id
+    req.storyModel.remove({ _id: idParam }, (err, removedStory) => {
+      if (err) console.error(err)
+      res.json(removedStory)
+    })
+  })
+}
